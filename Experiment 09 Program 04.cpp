70293232c0cc4ee3d90283d8d246aa58b30f441e
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-08-13 13:55:18 +0530

    Experiment 09 Program 04.cpp

---
+++ Experiment 09 Program 04.cpp
@@ -0,0 +1,9 @@
+//Print a string using pointers
+#include<iostream>
+using namespace std;
+int main() {
+    char str[] = "Hello, I am Swarali Gurjar!";
+    char *ptr = str; 
+    cout << ptr;
+    return 0;
+}
